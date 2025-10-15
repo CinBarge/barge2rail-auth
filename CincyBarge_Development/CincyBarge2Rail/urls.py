afacfefeed0e1fce0a0e14611a784251fdfa78a6
Brandon P <brandonphrathep@gmail.com>
2025-10-15 15:18:10 -0400

    Removed CincyBarge_Development Login. All login and redirections for user authentication is done through the SSO

--- CincyBarge_Development/CincyBarge2Rail/urls.py
+++ CincyBarge_Development/CincyBarge2Rail/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('sso-login/', user_view.sso_login, name='sso-login'),
     path('register/', user_view.register, name='user-register'),
     path('profile/', user_view.profile, name='user-profile'),
-    path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
 
 ]
